66c3689d0cff3aa4100b582a592c20b11b27222b
DAVMCKII <mckdav98@gmail.com>
2022-06-21 11:27:13 -0400

    created table models for the database : ;

---
+++ models/index.js
@@ -0,0 +1,34 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+Product.belongsTo(Category, {
+  foreignKey:'category_id',
+  });
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id'
+});
+
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'products',
+  foreignKey: 'product_id'
+});
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
